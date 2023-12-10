9edb9caaaa87ad58e3d1dee18d9d9495ba79a3cd
Daniel <129618322+dayeya@users.noreply.github.com>
2023-12-10 19:30:49 +0200

    added main function.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from app.website import create_app
 
-if __name__ == '__main__':
+def main():
     app = create_app()
     app.run(host='127.0.0.1', port=50000, debug=True)
+    
+if __name__ == '__main__':
+    main()
