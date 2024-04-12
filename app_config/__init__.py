33706b1c877bc9c346b4a4599dd65c23fb9e187b
Daniel <clipchamp.daniel@gmail.com>
2024-04-12 12:54:36 +0300

    Adding basic configuration setup.

---
+++ app_config/__init__.py
@@ -0,0 +1,1 @@
+from .ini import set_global_parser, get_configuration_from_section
---
+++ app_config/ini.py
@@ -0,0 +1,54 @@
+import pathlib as plib
+import configparser as parser
+from configparser import SectionProxy
+from typing import Iterable
+
+global PARSER
+PARSER: parser.ConfigParser = None
+
+ROOT_DIR = plib.Path(__file__).parent
+
+
+class SectionDoesNotExist(Exception):
+    pass
+
+
+class ValueNotFound(Exception):
+    pass
+
+
+def chain_path(file: plib.Path) -> plib.Path:
+    return ROOT_DIR.joinpath(file)
+
+
+def set_global_parser() -> None:
+    """
+    Sets the global parser.
+    Returns: None
+    """
+    global PARSER
+
+    if not PARSER:
+        PARSER = parser.ConfigParser()
+
+
+def get_configuration_from_section(section_name: str, file: plib.Path, keys: Iterable=None):
+    try:
+        section = get_section(section_name, file=file)
+        return section if not keys else [section[item] for item in keys]
+    except KeyError as _e:
+        raise ValueNotFound(f"Some values of {keys} don't exit in the specified file object or {section_name}.")
+
+
+def get_section(section_name: str, file: plib.Path) -> SectionProxy:
+    global PARSER
+
+    if not PARSER:
+        set_global_parser()
+
+    PARSER.read(chain_path(file))
+    if not PARSER.has_section(section_name):
+        raise SectionDoesNotExist(f"{file} does not contain {section_name} as a section.")
+
+    return PARSER[section_name]
+
